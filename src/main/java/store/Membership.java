83a796c4a4bc15aa41b0b84056b2990a9e670fec
Youmee <137977582+YoumeePark@users.noreply.github.com>
2024-11-09 23:07:34 +0900

    Membership.java

---
+++ src/main/java/store/Membership.java
@@ -0,0 +1,9 @@
+public class Membership {
+    private static final double DISCOUNT_RATE = 0.30;
+    private static final int MAX_DISCOUNT = 8000;
+
+    public int applyDiscount(int amount) {
+        int discount = (int) (amount * DISCOUNT_RATE);
+        return Math.min(discount, MAX_DISCOUNT);
+    }
+}
